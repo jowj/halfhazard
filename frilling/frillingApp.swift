e391b4972894bd59633370ee87a272b0ffdd316b
josiah <me@jowj.net>
2024-02-24 12:25:29 -0600

    Initial Commit

---
+++ frilling/frillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  frillingApp.swift
+//  frilling
+//
+//  Created by Josiah Ledbetter on 2024-02-24.
+//
+
+import SwiftUI
+
+@main
+struct frillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
