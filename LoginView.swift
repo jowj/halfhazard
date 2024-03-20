2ff54e1989b0c7893a3f6ca3e9311f3853e0e287
josiah <me@jowj.net>
2024-03-20 09:51:36 -0500

    Add basic login/logout functionality.

--- LoginView.swift
+++ LoginView.swift
@@ -18,57 +18,71 @@ struct LoginView: View {
     
     var body: some View {
         VStack {
-            Text("Login. or don't. i'm not your mom.!")
             
-            Spacer()
-            
-//            Table(of: user.self) {
-//                TableColumn("user id") {
-//                    Text(
-//                }
-//            }
-            
-            Spacer()
-            
-            SignInWithAppleButton(.continue) { request in
+            if userID.isEmpty {
+                Text("Login. or don't. i'm not your mom.!")
+
+                Spacer()
                 
-                request.requestedScopes = [.email, .fullName]
-                
-            } onCompletion: { result in
-                switch result {
-                case .success(let auth):
-                    switch auth.credential {
-                    case let cred as ASAuthorizationAppleIDCredential:
-                        // Actually important!!
-                        let userID = cred.user
-                        
-                        // Just misc stuff, but if you don't save it after they auth one time you never get it again!!
-                        // Store in your DB.
-                        let email = cred.email
-                        let firstName = cred.fullName?.givenName
-                        let lastName = cred.fullName?.familyName
-                        
-                        // assigns these values to local storage, which is important, i guess?
-                        self.email = email ?? ""
-                        self.userID = userID
-                        self.firstName = firstName ?? ""
-                        self.lastName = lastName ?? ""
-                        
-                    default:
-                        break
+                SignInWithAppleButton(.continue) { request in
+                    
+                    request.requestedScopes = [.email, .fullName]
+                    
+                } onCompletion: { result in
+                    switch result {
+                    case .success(let auth):
+                        switch auth.credential {
+                        case let cred as ASAuthorizationAppleIDCredential:
+                            // Actually important!!
+                            let userID = cred.user
+                            
+                            // Just misc stuff, but if you don't save it after they auth one time you never get it again!!
+                            // Store in your DB.
+                            let email = cred.email
+                            let firstName = cred.fullName?.givenName
+                            let lastName = cred.fullName?.familyName
+                            
+                            // assigns these values to local storage, which is important, i guess?
+                            self.email = email ?? ""
+                            self.userID = userID
+                            self.firstName = firstName ?? ""
+                            self.lastName = lastName ?? ""
+                            
+                        default:
+                            break
+                        }
+                    case .failure(let error):
+                        print(error)
                     }
-                case .failure(let error):
-                    print(error)
                 }
+                .signInWithAppleButtonStyle(
+                    colorScheme == .dark ? .white : .black
+                )
+            } else {
+                Text("You are logged in!")
+                List {
+                    Text("UserID is \(userID)")
+                    Text("email is \(email)")
+                }
+                Button(role: .destructive) {
+                    withAnimation {
+                        userID = ""
+                        // There HAS to be a better way of reasoning about this, right?? 
+                    }
+                } label: {
+                    Label("Logout", systemImage: "rectangle.portrait.and.arrow.right")
+                        .symbolVariant(.circle.fill)
+                }
+
             }
-            .signInWithAppleButtonStyle(
-                colorScheme == .dark ? .white : .black
-            )
+            
+            Spacer()
+
             .frame(height: 50)
             .padding()
             .cornerRadius(8)
         }
-        .navigationTitle("Sign In")
+        .navigationTitle("Account Info")
     }
 }
     
