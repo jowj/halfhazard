e64a4d2b49a27df6a5331c1e5b38aa5b2487042b
josiah <me@jowj.net>
2024-04-13 12:05:45 -0500

    Further default value changes.
    Some of my original defs didn't actual provide appropriate default
    values for optional arrays of a specific type.

--- halfhazard/Models/User.swift
+++ halfhazard/Models/User.swift
@@ -12,14 +12,19 @@ import SwiftData
 @Model
 class User: Identifiable {
     
-    let userID: String
-    var name: String?
+    var userID: String = ""
+    var name: String? = ""
 
-    @Relationship(deleteRule: .nullify, inverse: \Group.members) var groups: [Group]? // users won't necessarily have groups
-    var expenses: [Expense]? // new users won't have any expenses
+    @Relationship(deleteRule: .nullify, inverse: \Group.members)
+        var groups: [Group]? = [Group]() // users won't necessarily have groups
+    
+    var expenses: [Expense]? = [Expense]() // new users won't have any expenses
         
-    init(userID: String) {
+    init(userID: String, name: String? = nil, groups: [Group]? = [Group](), expenses: [Expense]? = [Expense]()) {
         self.userID = userID
+        self.name = name
+        self.groups = groups
+        self.expenses = expenses
     }
 }
 
