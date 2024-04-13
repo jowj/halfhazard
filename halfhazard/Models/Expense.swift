8331c980a2b9318915dd0fb05893bfd739967005
josiah <me@jowj.net>
2024-04-13 12:40:36 -0500

    Add further default values.
    I end up having to have BOTH var definition time default value AND
    init time default value here. I'm not exactly sure why, but if I only
    use the var def time then I lose the ability to define them at
    runtime, which is obviously not good, but I think I also want them to
    have SOMETHING there, even if they are overwritten, because I suspect
    it impacts the cloudkit integration.
    
    Annoying.

--- halfhazard/Models/Expense.swift
+++ halfhazard/Models/Expense.swift
@@ -14,12 +14,14 @@ import SwiftData
 class Expense: Identifiable {
     // A class that models all the metadata we might want attached to /an expense/.
     let expenseID = UUID()
-    var name: String
-    var amount: Double
+    var name: String = ""
+    var amount: Double = 0.0
     var author: User?
     var group: Group?
-    var isCompleted: Bool
-    var timestamp: Date
+    var isCompleted: Bool = false
+    var timestamp: Date = Date.now
+    
+    @Relationship(deleteRule: .nullify, inverse: \ExpenseCategory.items)  var category: ExpenseCategory? = nil // this line differs from tutorials because the deleteRule label didn't used to be necessary.
     
     var amountOwed: Double? {
         // I'll have to figure out a split percentage deal, too, but for now just focus on even splits.
@@ -34,17 +36,8 @@ class Expense: Identifiable {
         }
         
     }
-    
-    @Relationship(deleteRule: .nullify, inverse: \ExpenseCategory.items)  var category: ExpenseCategory? // this line differs from tutorials because the deleteRule label didn't used to be necessary.
-    
-    init(name: String = "",
-         amount: Double = 0.0,
-         isCompleted: Bool = false,
-         timestamp: Date = .now
-    ) {
+    init(name: String = "", amount: Double = 0.0) {
         self.name = name
         self.amount = amount
-        self.isCompleted = isCompleted
-        self.timestamp = timestamp
     }
 }
