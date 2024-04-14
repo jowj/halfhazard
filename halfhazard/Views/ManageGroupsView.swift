9b4eb3b1939b77e26c46d9f4eac4e669f63aef14
josiah <me@jowj.net>
2024-04-14 09:14:26 -0500

    Actually set the current user as a member of a group upon create.

--- halfhazard/Views/ManageGroupsView.swift
+++ halfhazard/Views/ManageGroupsView.swift
@@ -12,55 +12,61 @@ struct ManageGroupsView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) var modelContext
     
+    // Needed to set group membership
+    @AppStorage("userID") var userID: String = ""
+    @Query private var users: [User]
+
+    
     @State private var groupName: String = ""
     @Query private var groups: [Group]
     
     var body: some View {
-        
-        List {
-
-            TextField("What is your group name?",
-                      text: $groupName)
-            Button("Add Group") {
-                withAnimation {
-                    save()
+        NavigationStack {
+            List {
+                
+                TextField("What is your group name?",
+                          text: $groupName)
+                Button("Add Group") {
+                    withAnimation {
+                        save()
+                    }
                 }
-            }
-            .disabled(groupName.isEmpty)
-            .tint(.green)
-            
-            Section("Existing Groups") {
-                if groups.isEmpty {
+                .disabled(groupName.isEmpty)
+                .tint(.green)
+                
+                Section("Existing Groups") {
+                    if groups.isEmpty {
                         HStack {
                             ContentUnavailableView("No groups exist.",
-                            systemImage: "archivebox")
+                                                   systemImage: "archivebox")
                         }
-                } else {
-                    ForEach(groups) { group in
-                        HStack {
-                            Text(group.name)
-                                .contextMenu {
-                                    Button(role: .destructive) {
-                                        withAnimation {
-                                            modelContext.delete(group)
+                    } else {
+                        ForEach(groups) { group in
+                            HStack {
+                                Text(group.name)
+                                    .contextMenu {
+                                        Button(role: .destructive) {
+                                            withAnimation {
+                                                modelContext.delete(group)
+                                            }
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
+                                                .symbolVariant(.circle.fill)
                                         }
-                                    } label: {
-                                        Label("Delete", systemImage: "trash")
-                                            .symbolVariant(.circle.fill)
                                     }
-                                }
+                            }
                         }
                     }
                 }
+                
             }
-
-        }
-        .navigationTitle("Manage groups")
-        .toolbar {
-            
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Dismiss") {
-                    dismiss()
+            .navigationTitle("Manage groups")
+            .toolbar {
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
                 }
             }
         }
@@ -71,11 +77,11 @@ private extension ManageGroupsView {
     
     func save() {
         let group = Group(name: groupName)
+        group.members = [currentUser(users: users, currentUserID: userID)]
         modelContext.insert(group)
         // I'm not sure why we have to set category to empty array here
         // but title must be reset to "" or you live with some weird erros
         // and a fucked up UI.
-        group.members = []
         groupName = ""
     }
     
