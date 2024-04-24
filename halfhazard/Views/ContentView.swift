10e08f22fca9c1771d106f719ff77188b16ef694
josiah <me@jowj.net>
2024-04-24 11:07:48 -0500

    Correctly handle logged in / logged out with navstack.

--- halfhazard/Views/ContentView.swift
+++ halfhazard/Views/ContentView.swift
@@ -15,9 +15,9 @@ struct ContentView: View {
     @AppStorage("email") var email: String = ""
     @AppStorage("fullName") var fullName: String = ""
     @AppStorage("userID") var userID: String = ""
-
+    
     var loggedIn = false
-        
+    
     @State private var showCreate = false
     @State private var showCreateCategory = false
     @State private var showAccountDetails = false
@@ -73,121 +73,121 @@ struct ContentView: View {
             return [Group]()
         }
     }
-
+    
     
     var body: some View {
-//        NavigationStack {
-//            if userID.isEmpty {
-//                LoginView()
-//            } else {
-//                HomeView()
-//                
-//            }
-//        }
-        
-        NavigationSplitView {
-            List(filteredGroups, selection: $selectedGroup) { group in
-                // show me a list of all groups!
-                NavigationLink("\(group.name)", value: group)
-            }
-        } content: {
-            // Show me the list of expenses under a selected group.
-            if let activeGroup = selectedGroup.self {
-                GroupView(selectedGroup: activeGroup)
-            }
-        } detail: {
-            // Show me all details about the selected expense!
-            //ExpenseView(expense: <#T##Expense#>)
-        }
-        .navigationTitle("Your finances are halfhazard")
-        .searchable(text: $searchQuery,
-                    prompt: "Search for an expense")
-        // This Tool bar section configures just the tool bar buttons
-        .toolbar {
-            ToolbarItem {
-                Button {
-                    showCreate.toggle()
-                } label: {
-                    Label("Add Item", systemImage: "plus")
-                }
-                
-            }
-            ToolbarItem {
-                Button {
-                    showCreateCategory.toggle()
-                } label: {
-                    Label("Manage Categories", systemImage: "ellipsis")
-                }
-                
-            }
-            ToolbarItem {
-                Button {
-                    showManageGroups.toggle()
-                } label: {
-                    Label("Manage Groups", systemImage: "person.3.fill")
-                }
-            }
-            ToolbarItem {
-                Button {
-                    showAccountDetails.toggle()
-                } label: {
-                    Label("Login", systemImage: "person")
-                }
-                
-            }
-            
-            
-        } // THese .sheets are all related to the toolbar stuff above.
-        .sheet(isPresented: $showCreate,
-               content: {
-            NavigationStack {
-                CreateExpenseView()
-#if os(macOS)
-                    .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-                
-            }
-            // stolen from https://stackoverflow.com/questions/66216468/how-to-make-a-swiftui-sheet-size-match-the-width-height-of-window-on-macos
-        })
-        .sheet(isPresented: $showCreateCategory,
-               content: {
-            NavigationStack {
-                CreateCategoryView()
-#if os(macOS)
-                    .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-            }
-        })
-        .sheet(isPresented: $showManageGroups,
-               content: {
-            NavigationStack {
-                ManageGroupsView()
-#if os(macOS)
-                    .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-                
-            }
-        })
-        .sheet(isPresented: $showAccountDetails,
-               content: {
+        if userID.isEmpty {
             NavigationStack {
                 LoginView()
+                
+            }
+        } else {
+            NavigationSplitView {
+                List(filteredGroups, selection: $selectedGroup) { group in
+                    // show me a list of all groups!
+                    NavigationLink("\(group.name)", value: group)
+                }
+            } content: {
+                // Show me the list of expenses under a selected group.
+                if let activeGroup = selectedGroup.self {
+                    GroupView(selectedGroup: activeGroup)
+                }
+            } detail: {
+                // Show me all details about the selected expense!
+                //ExpenseView(expense: <#T##Expense#>)
+            }
+            .navigationTitle("Your finances are halfhazard")
+            .searchable(text: $searchQuery,
+                        prompt: "Search for an expense")
+            // This Tool bar section configures just the tool bar buttons
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        showCreate.toggle()
+                    } label: {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                    
+                }
+                ToolbarItem {
+                    Button {
+                        showCreateCategory.toggle()
+                    } label: {
+                        Label("Manage Categories", systemImage: "ellipsis")
+                    }
+                    
+                }
+                ToolbarItem {
+                    Button {
+                        showManageGroups.toggle()
+                    } label: {
+                        Label("Manage Groups", systemImage: "person.3.fill")
+                    }
+                }
+                ToolbarItem {
+                    Button {
+                        showAccountDetails.toggle()
+                    } label: {
+                        Label("Login", systemImage: "person")
+                    }
+                    
+                }
+                
+                
+            } // THese .sheets are all related to the toolbar stuff above.
+            .sheet(isPresented: $showCreate,
+                   content: {
+                NavigationStack {
+                    CreateExpenseView()
+#if os(macOS)
+                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+#endif
+                    
+                }
+                // stolen from https://stackoverflow.com/questions/66216468/how-to-make-a-swiftui-sheet-size-match-the-width-height-of-window-on-macos
+            })
+            .sheet(isPresented: $showCreateCategory,
+                   content: {
+                NavigationStack {
+                    CreateCategoryView()
+#if os(macOS)
+                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+#endif
+                }
+            })
+            .sheet(isPresented: $showManageGroups,
+                   content: {
+                NavigationStack {
+                    ManageGroupsView()
+#if os(macOS)
+                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+#endif
+                    
+                }
+            })
+            .sheet(isPresented: $showAccountDetails,
+                   content: {
+                NavigationStack {
+                    LoginView()
+#if os(macOS)
+                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+#endif
+                    
+                }
+            })
+            
+            .sheet(item: $expenseEdit) {
+                expenseEdit = nil
+            } content: {item in
+                EditExpenseView(item: item)
 #if os(macOS)
                     .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
 #endif
                 
             }
-        })
-        
-        .sheet(item: $expenseEdit) {
-            expenseEdit = nil
-        } content: {item in
-            EditExpenseView(item: item)
-#if os(macOS)
-                .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
             
         }
-
+        
     }
+    
 }
