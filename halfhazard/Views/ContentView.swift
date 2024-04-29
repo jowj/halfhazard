e4fccecfd34cca396c0831f6aef9c001e64fd5c5
josiah <me@jowj.net>
2024-04-29 09:21:04 -0500

    Fix toolbar behavior cross platform!
    I only had to redeclare the idiot toolbar shit and leave two calls
    each. MacOS seems to expect that you place the toolbar OUTSIDE the
    navigationsplitview(), and ios expects that you do it *INSIDE*.
    
    What the fuck apple.

--- halfhazard/Views/ContentView.swift
+++ halfhazard/Views/ContentView.swift
@@ -49,8 +49,6 @@ struct ContentView: View {
                 LoginView()
             }
         } else {
-//            LoginView()
-
             NavigationSplitView {
                 
                 List(filteredGroups, selection: $selectedGroup) { group in
@@ -58,6 +56,81 @@ struct ContentView: View {
                     NavigationLink("\(group.name)", value: group)
                         //.toolbar(removing: .sidebarToggle)
                 }
+                .navigationTitle("Your finances are halfhazard")
+                .searchable(text: $searchQuery,
+                            prompt: "Search for an expense")
+                // This Tool bar section configures just the tool bar buttons
+                .toolbar {
+                    ToolbarItemGroup {
+                        Button {
+                            showCreate.toggle()
+                        } label: {
+                            Label("Add Item", systemImage: "plus")
+                        }
+                        
+                        Button {
+                            showCreateCategory.toggle()
+                        } label: {
+                            Label("Manage Categories", systemImage: "ellipsis")
+                        }
+                        
+                        Button {
+                            showManageGroups.toggle()
+                        } label: {
+                            Label("Manage Groups", systemImage: "person.3.fill")
+                        }
+                        Button {
+                            showAccountDetails.toggle()
+                        } label: {
+                            Label("Login", systemImage: "person")
+                        }
+                    }
+                
+                    
+                    
+                } // THese .sheets are all related to the toolbar stuff above
+                .sheet(isPresented: $showCreate,
+                       content: {
+                    NavigationStack {
+                        CreateExpenseView()
+    #if os(macOS)
+                            .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+    #endif
+                        
+                    }
+                    // stolen from https://stackoverflow.com/questions/66216468/how-to-make-a-swiftui-sheet-size-match-the-width-height-of-window-on-macos
+                })
+                .sheet(isPresented: $showCreateCategory,
+                       content: {
+                    NavigationStack {
+                        CreateCategoryView()
+    #if os(macOS)
+                            .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+    #endif
+                    }
+                })
+                .sheet(isPresented: $showManageGroups,
+                       content: {
+                    NavigationStack {
+                        ManageGroupsView()
+    #if os(macOS)
+                            .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+    #endif
+                        
+                    }
+                })
+                .sheet(isPresented: $showAccountDetails,
+                       content: {
+                    NavigationStack {
+                        LoginView()
+    #if os(macOS)
+                            .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
+    #endif
+                        
+                    }
+                })
+
+
             } detail: {
                 Text("Detail view worked!")
                 // Show me all the expenses inside a group
@@ -65,87 +138,35 @@ struct ContentView: View {
                     GroupView(selectedGroup: activeGroup)
                 }
             }
-            
-            .navigationTitle("Your finances are halfhazard")
-            .searchable(text: $searchQuery,
-                        prompt: "Search for an expense")
-            // This Tool bar section configures just the tool bar buttons
             .toolbar {
-                ToolbarItem {
+                ToolbarItemGroup {
                     Button {
                         showCreate.toggle()
                     } label: {
                         Label("Add Item", systemImage: "plus")
                     }
                     
-                }
-                ToolbarItem {
                     Button {
                         showCreateCategory.toggle()
                     } label: {
                         Label("Manage Categories", systemImage: "ellipsis")
                     }
                     
-                }
-                ToolbarItem {
                     Button {
                         showManageGroups.toggle()
                     } label: {
                         Label("Manage Groups", systemImage: "person.3.fill")
                     }
-                }
-                ToolbarItem {
                     Button {
                         showAccountDetails.toggle()
                     } label: {
                         Label("Login", systemImage: "person")
                     }
-                    
                 }
+            
                 
                 
-            } // THese .sheets are all related to the toolbar stuff above.
-            .sheet(isPresented: $showCreate,
-                   content: {
-                NavigationStack {
-                    CreateExpenseView()
-#if os(macOS)
-                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-                    
-                }
-                // stolen from https://stackoverflow.com/questions/66216468/how-to-make-a-swiftui-sheet-size-match-the-width-height-of-window-on-macos
-            })
-            .sheet(isPresented: $showCreateCategory,
-                   content: {
-                NavigationStack {
-                    CreateCategoryView()
-#if os(macOS)
-                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-                }
-            })
-            .sheet(isPresented: $showManageGroups,
-                   content: {
-                NavigationStack {
-                    ManageGroupsView()
-#if os(macOS)
-                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-                    
-                }
-            })
-            .sheet(isPresented: $showAccountDetails,
-                   content: {
-                NavigationStack {
-                    LoginView()
-#if os(macOS)
-                        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity, alignment: .center)
-#endif
-                    
-                }
-            })
-                
+            }
         }
         
     }
